f1e30ba97441fdb06e32608789f14b8e3f895bfc
James Cook <jcook@proctoru.com>
2020-09-30 06:07:15 -0500

    Rework the node matching logic to also target 'change_column'. Remove matchers for 'create_table'.

--- style/ruby/extensions/unsafe_migration.rb
+++ style/ruby/extensions/unsafe_migration.rb
@@ -3,51 +3,32 @@ module RuboCop
     module Migration
       class UnsafeMigration < Cop
         MSG = <<~HEREDOC
-          Please verify a default value is safe to apply in this instance.
-          Ask in #database / #devops
+          Please verify a default column value is safe to apply in this instance.
+          Adding default values to large tables in production can cause downtime.
         HEREDOC
-
-        # This matcher is meant to run against a child node of create_table
-        def_node_matcher :_create_table_with_default_column_value?, <<~PATTERN
-          (hash <$(pair (sym :default) !nil) ...>)
-        PATTERN
 
         def_node_matcher :add_column_default_option?, <<~PATTERN
           (send nil? :add_column _ _ _ (hash <$(pair (sym :default) !nil) ...>))
         PATTERN
 
-        def_node_matcher :create_table_with_block?, <<~PATTERN
-          (block
-            (send nil? :create_table ...)
-            (args (arg _var))
-            _)
+        def_node_matcher :change_column_default_option?, <<~PATTERN
+          (send nil? :change_column _ _ _ (hash <$(pair (sym :default) !nil) ...>))
         PATTERN
 
-        def create_table_with_default_column_value?(node)
-          return false unless node
-
-          if _create_table_with_default_column_value?(node)
-            return true
-          end
-
-          if node && node.respond_to?(:children) && node.children.any?
-            node.children.each do |child| 
-              if create_table_with_default_column_value?(child)
-                add_offense(child)
-              end
-            end
-          end
-
-          false
+        # Attempt to ignore 'down' methods
+        def up_or_change_method?(node)
+          return false unless node.parent && node.parent.parent
+          return true unless node.parent.parent.respond_to?(:method_name)
+          node.parent.parent.method_name == :up || node.parent.parent.method_name == :change
         end
 
         def on_send(node, *args)
-          if add_column_default_option?(node)
+          if add_column_default_option?(node) && up_or_change_method?(node)
             add_offense(node)
           end
 
-          if create_table_with_block?(node.parent)
-            create_table_with_default_column_value?(node.parent)
+          if change_column_default_option?(node) && up_or_change_method?(node)
+            add_offense(node)
           end
         end
       end
